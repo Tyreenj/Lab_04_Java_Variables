a9927a3f8b0620300dc0228b7e3ae54776b5ec2e
Noah Tyree <noahjtyree@gmail.com>
2024-01-29 18:09:54 -0500

    first commit

---
+++ src/Declarations.java
@@ -0,0 +1,13 @@
+public class Declarations {
+    public static void main(String[] args)
+    {
+        double lunchCost = 14.4;
+        int numOfKids = 2;
+        String isRaining = "Yes";
+        double gasPrice = 2.97;
+        int favNum = 14;
+        double shoeSize = 7;
+        String birthMonth = "July";
+        String fullName = "Noah Jonathan Tyree";
+    }
+}
